3250053704ed8649c7e736ee43042e98a9b76ce7
ljyockey <linyockey@gmail.com>
2017-04-09 13:50:57 -0700

    fixed nesting issues

--- js/app.js
+++ js/app.js
@@ -3,28 +3,28 @@ var currentMove = '';
 
 function displayMove() {
 	$('.row').on('click', '.square', function() {
-	var moveClass = this.className;	
-	if (counter % 2 === 0) {	
-		$(this).html('<p class="x">X</p>');
-		currentMove = 'X';
-		} 
-	else {
-		$(this).html('<p class="o">O</p>');
-		currentMove = 'O';
-		}	
-	counter++;		
-	getClassArray(moveClass);
-	if (counter === 9) {
-		declareTie();
+		var moveClass = this.className;	
+		if (counter % 2 === 0) {	
+			$(this).html('<p class="x">X</p>');
+			currentMove = 'X';
+			} 
+		else {
+			$(this).html('<p class="o">O</p>');
+			currentMove = 'O';
+			}	
+		counter++;		
+		getClassArray(moveClass);
+		if (counter === 9) {
+			declareTie();
+		}
+		});
 	}
-});
-}
 
 function getClassArray(string) {
 	var classArray = string.split(' ');
 	classArray.splice(0, 1);
 	getRows(classArray);
-}
+	}
 
 function getRows(token) {
 	token.forEach(function(item) {
@@ -38,26 +38,26 @@ function validateWin(validator, move, targetClass) {
 	for (var i = 0; i < validator.length; i++) {
 		if (validator[i].innerText.trim() === move) {
 			pointCounter++;
+			}
 		}
-	}
 	if (pointCounter === 3) {
 		declareWinner(targetClass);
+		}
 	}
-}
 
 function declareWinner(c) {
 	$('.square').each(function() {
 		if ($(this).hasClass(c)) {
 			$(this).addClass('winner');
 		}
-	});
-}
+		});
+	}
 
 function declareTie() {
 	if (!$('.square').hasClass('winner')) {
-	$('.tie').show();
-}
-}
+		$('.tie').show();
+		}
+	}
 
 function restart() {
 	$('.restart').on('click', 'button', function(e) {
